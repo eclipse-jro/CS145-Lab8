521492a9e6924d26836ec4a66283ff31f6e8fc8e
Jose Rene Ortega <ortegaj2@LabImage2016-17>
2019-03-15 17:56:33 -0700

    BinarySearch works for out-of-bounds cases as well

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -6,29 +6,41 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 public class BinarySearch {
+    // highIndex - the maximum index of the part of the array being searched
+    // lowIndex - the minimum index of the part of the array being searched
+    // target - the value being searched for
     public int search(int[] intsForSearch, int highIndex, int lowIndex, int target) {
-        // highIndex - the maximum index of the part of the array being searched
-        // lowIndex - the minimum index of the part of the array being searched
-        // target - the value being searched for
 
         int result = -1;
 
         // How "middleIndex" is defined
-        int middleIndex = (highIndex + lowIndex)/2;
+        int middleIndex = (int)((highIndex + lowIndex)/2);
 
         // return the middle index value if it equals the target
         if (intsForSearch[middleIndex] == target) {
-            return intsForSearch[middleIndex];
-        }
-
-        // logic to decide to search in the first or second half of the list next
-        if (intsForSearch[middleIndex] > target) {
-            // search first half of the list
-            result = search(intsForSearch, lowIndex, middleIndex - 1, target);
+            return middleIndex;
         }
         else {
-            // search second half of the list
-            result = search(intsForSearch, middleIndex + 1, highIndex, target);
+            // logic to decide to search in the first or second half of the list next
+            if (intsForSearch[middleIndex] > target) {
+
+                // if target is less than the value at teh lowIndex then return -1
+                if (intsForSearch[lowIndex] > target) {
+                    return result;
+                }
+
+                // search first half of the list
+                result = search(intsForSearch, middleIndex - 1, lowIndex, target);
+            } else if (intsForSearch[middleIndex] < target) {
+
+                // if target is more than the value at the highIndex then return -1
+                if (intsForSearch[highIndex] < target) {
+                    return result;
+                }
+                
+                // search second half of the list
+                result = search(intsForSearch, highIndex, middleIndex + 1, target);
+            }
         }
 
     return result;
